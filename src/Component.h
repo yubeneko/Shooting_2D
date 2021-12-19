1609e74f7623db6f0f2f770b78410303c030f9f6
yubeneko <ryohei2674@gmail.com>
2021-12-19 14:24:04 +0900

    [add] Component クラスを作成。

---
+++ src/Component.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdint>
+
+class Component
+{
+public:
+	Component(class Actor* owner, int updateOrder = 100);
+	virtual ~Component();
+
+	virtual void Update(float deltaTime);
+	virtual void ProcessInput(const uint8_t* keyState) {}
+	virtual void OnUpdateWorldTransform() {}
+
+	int GetUpdateOrder() const { return mUpdateOrder; }
+
+protected:
+	class Actor* mOwner;
+	int mUpdateOrder;
+};
