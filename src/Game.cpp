a412455fb19a62cfe88c2918843f957961f38d8d
yubeneko <ryohei2674@gmail.com>
2021-12-19 14:24:36 +0900

    [add] Game クラスを作成

---
+++ src/Game.cpp
@@ -0,0 +1,172 @@
+#include "Game.h"
+#include "Renderer.h"
+#include "Actor.h"
+#include <algorithm>
+
+Game::Game()
+  : mRenderer(nullptr),
+	mIsRunning(true),
+	mUpdatingActors(false)
+{
+}
+
+bool Game::Initialize()
+{
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+	{
+		SDL_Log("Unable to initialize SDL : %s", SDL_GetError());
+		return false;
+	}
+
+	mRenderer = new Renderer(this);
+	if (!mRenderer->Initialize(1024.0f, 768.0f))
+	{
+		SDL_Log("Failed to initialize renderer");
+		delete mRenderer;
+		mRenderer = nullptr;
+		return false;
+	}
+
+	LoadData();
+
+	mTicksCount = SDL_GetTicks();
+
+	return true;
+}
+
+void Game::RunLoop()
+{
+	while (mIsRunning)
+	{
+		ProcessInput();
+		UpdateGame();
+		GenerateOutput();
+	}
+}
+
+void Game::Shutdown()
+{
+	UnloadData();
+	if (mRenderer) { mRenderer->Shutdown(); }
+	SDL_Quit();
+}
+
+void Game::AddActor(Actor* actor)
+{
+	if (mUpdatingActors)
+	{
+		mPendingActors.emplace_back(actor);
+	}
+	else
+	{
+		mActors.emplace_back(actor);
+	}
+}
+
+void Game::RemoveActor(Actor* actor)
+{
+	auto iter = std::find(mPendingActors.begin(), mPendingActors.end(), actor);
+	if (iter != mPendingActors.end())
+	{
+		std::iter_swap(iter, mPendingActors.end() - 1);
+		mPendingActors.pop_back();
+	}
+
+	auto iter = std::find(mActors.begin(), mActors.end(), actor);
+	if (iter != mActors.end())
+	{
+		std::iter_swap(iter, mActors.end() - 1);
+		mActors.pop_back();
+	}
+}
+
+void Game::ProcessInput()
+{
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+			case SDL_QUIT:
+				mIsRunning = false;
+				break;
+		}
+	}
+
+	const uint8_t* state = SDL_GetKeyboardState(NULL);
+	if (state[SDL_SCANCODE_ESCAPE])
+	{
+		mIsRunning = false;
+	}
+
+	for (auto actor : mActors)
+	{
+		actor->ProcessInput(state);
+	}
+}
+
+void Game::UpdateGame()
+{
+	// 前回のフレームから16ms経過するまで待機
+	// つまり、目標のフレームレートは60FPS
+	while (!SDL_TICKS_PASSED(SDL_GetTicks(), mTicksCount + 16))
+		;
+
+	float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f;
+	if (deltaTime > 0.05f)
+	{
+		deltaTime = 0.05f;
+	}
+	mTicksCount = SDL_GetTicks();
+
+	mUpdatingActors = true;
+	for (auto actor : mActors)
+	{
+		actor->Update(deltaTime);
+	}
+	mUpdatingActors = false;
+
+	for (auto pending : mPendingActors)
+	{
+		pending->ComputeWorldTransform();
+		mActors.emplace_back(pending);
+	}
+	mPendingActors.clear();
+
+	std::vector<Actor*> deadActors;
+	for (auto actor : mActors)
+	{
+		if (actor->GetState() == Actor::EDead)
+		{
+			deadActors.emplace_back(actor);
+		}
+	}
+
+	for (auto actor : deadActors)
+	{
+		delete actor;
+	}
+}
+
+void Game::GenerateOutput()
+{
+	mRenderer->Draw();
+}
+
+void Game::LoadData()
+{
+
+}
+
+void Game::UnloadData()
+{
+	while(!mActors.empty())
+	{
+		delete mActors.back();
+	}
+
+	if (mRenderer)
+	{
+		mRenderer->UnloadData();
+	}
+}
---
+++ src/Game.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <SDL.h>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Game
+{
+public:
+	Game();
+	bool Initialize();
+	void RunLoop();
+	void Shutdown();
+
+	void AddActor(class Actor* actor);
+	void RemoveActor(class Actor* actor);
+
+	class Renderer* GetRenderer() const { return mRenderer; }
+
+private:
+	void ProcessInput();
+	void UpdateGame();
+	void GenerateOutput();
+
+	void LoadData();
+	void UnloadData();
+
+	std::vector<class Actor*> mActors;
+	std::vector<class Actor*> mPendingActors;
+
+	class Renderer* mRenderer;
+
+	Uint32 mTicksCount;
+	bool mIsRunning;
+	bool mUpdatingActors;
+};
