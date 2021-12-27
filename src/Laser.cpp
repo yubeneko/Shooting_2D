92f593637a6e396cdcdbbb5fb4956f7869249e12
yubeneko <ryohei2674@gmail.com>
2021-12-27 19:22:21 +0900

    [update] Laser クラスをアップデート。
    衝突判定と移動、スプライトを追加した。

--- src/Laser.cpp
+++ src/Laser.cpp
@@ -1,10 +1,19 @@
 #include "Laser.h"
 #include "CircleCollider.h"
+#include "EnemyShip.h"
+#include "SpriteComponent.h"
+#include "Game.h"
+#include "Renderer.h"
 
 Laser::Laser(Game* game)
   : Actor(game),
-	mDeathTimer(3.0f)
+	mDeathTimer(3.0f),
+	mSpeed(300.0f)
 {
+	SpriteComponent* sc = new SpriteComponent(this);
+	sc->SetTexture(GetGame()->GetRenderer()->GetTexture("Assets/Laser.png"));
+	CircleCollider* cc = new CircleCollider(this);
+	cc->SetRadius(10.0f);
 }
 
 void Laser::UpdateActor(float deltaTime)
@@ -13,12 +22,19 @@ void Laser::UpdateActor(float deltaTime)
 	if (mDeathTimer <= 0.0f)
 	{
 		SetState(EDead);
+		return;
 	}
+
+	glm::vec2 pos = GetPosition();
+	pos.x += mSpeed * deltaTime;
+	SetPosition(pos);
 }
 
 void Laser::OnCollision(CircleCollider* collider)
 {
-	if (collider->GetOwner()->GetTag() == "Enemy")
+	Actor* other = collider->GetOwner();
+	if (other->GetTag() == "Enemy")
 	{
+		other->SetState(EDead);
 	}
 }
--- src/Laser.h
+++ src/Laser.h
@@ -12,4 +12,5 @@ public:
 
 private:
 	float mDeathTimer;
+	float mSpeed;
 };
